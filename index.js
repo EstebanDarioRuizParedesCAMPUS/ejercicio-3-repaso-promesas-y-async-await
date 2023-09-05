7d0ef0754bcd9437ddcff9f778fb37725da1b74c
EstebanDarioRuizParedesCAMPUS <edrp.campus15@gmail.com>
2023-09-05 00:48:56 -0500

    ejecucion modo de promesas

--- index.js
+++ index.js
@@ -10,19 +10,28 @@ const readFilePro = (file) => {
     });
 };
 
-readFilePro(`${__dirname}/dog.txt`).then((result) => {
-    console.log(`Breed: ${result}`);
-    superagent
-        .get(`https://dog.ceo/api/breed/${result}/images/random`)
-        .then((res) => {
-            console.log(res.body.message);
-
-            fs.writeFile('dog-img.txt', res.body.message, (errorr) => {
-                if (errorr) return console.log(errorr.message);
-                console.log('random dog imag save to file');
-            });
-        })
-        .catch((err) => {
-            console.log(err.message);
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err) => {
+            if (err) reject("Couldn't no write the file");
+            resolve('success');
         });
-});
+    });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+    .then((result) => {
+        console.log(`Breed: ${result}`);
+        return superagent.get(`https://dog.ceo/api/breed/${result}/images/random`);
+    })
+    .then((res) => {
+        console.log(res.body.message);
+
+        return writeFilePro('dog-img.txt', res.body.message);
+    })
+    .then(() => {
+        console.log('random dog imag save to file');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
